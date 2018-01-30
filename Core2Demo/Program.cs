8696ea5bbe517241d68f60071f1ca5450d36c4a6
Sebastian Tecsi <set@addition.dk>
2018-01-30 10:18:40 +0200

    my changes

--- Core2Demo/Program.cs
+++ Core2Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace Core2Demo
 {
@@ -7,6 +8,30 @@ namespace Core2Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            Product product = new Product();
+            
+            product.Name = "Apple";
+            product.ExpiryDate = new DateTime(2008, 12, 28);
+            product.Price = 3.99M;
+            product.Sizes = new string[] { "Small", "Medium", "Large" };
+
+
+            string output = JsonConvert.SerializeObject(product);
+
+            Product deserializedProduct = JsonConvert.DeserializeObject<Product>(output);
+
+            Console.WriteLine("Hello World! " + deserializedProduct.Name);
+            Console.ReadLine();
         }
+    }
+
+    internal class Product
+    {
+        public string Name { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public decimal Price { get; set; }
+        public string[] Sizes { get; set; }
     }
 }
